41820365f9d8e31d961e7c3f36e83ee482b01740
Mahammad Irshad <86956934+Irshadmanglore@users.noreply.github.com>
2022-04-18 14:12:09 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+$con=mysqli_connect("sql300.epizy.com","epiz_31176939","0RRVbKpDgwiWOWi","epiz_31176939_FoodBiteDB");
+if(mysqli_connect_errno($con))
+{
+	echo "failed to connect to MySQL:" . mysqli_connect_error();
+}
+?>
+	
